2a1abb265002356b06b55e17f9dd69ec3b79371d
Blake Goodwin <bag3cn@virginia.edu>
2015-06-05 15:37:56 -0400

    Added scrolling, command-line feedback

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Aspose.Words for Java is an advanced class library for Java that enables you to perform 
+a great range of document processing tasks directly within your Java applications.
+
+Aspose.Words supports DOC, OOXML, RTF, HTML, OpenDocument, PDF, XPS, EPUB and other formats. 
+With Aspose.Words you can generate, modify, convert and render documents without using Microsoft Word.
+
+For more info see http://www.aspose.com/categories/java-components/aspose.words-for-java/default.aspx
+
--- src/appView.java
+++ src/appView.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -14,22 +20,18 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 
 /**
  * 
  * 
- * 			Document doc = new Document();
-			DocumentBuilder builder = new DocumentBuilder(doc);
-			
-			// Paragraph formatting - needs to be updated
-			ParagraphFormat paragraphFormat = builder.getParagraphFormat();
-			paragraphFormat.setFirstLineIndent(8);
-			paragraphFormat.setAlignment(ParagraphAlignment.JUSTIFY
-			
-			builder.writeln(fillIns.get(ans-1));
-			builder.writeln()
+ * Document doc = new Document(); DocumentBuilder builder = new
+ * DocumentBuilder(doc);
+ * 
+ * // Paragraph formatting - needs to be updated ParagraphFormat paragraphFormat
+ * = builder.getParagraphFormat(); paragraphFormat.setFirstLineIndent(8);
+ * paragraphFormat.setAlignment(ParagraphAlignment.JUSTIFY
+ * 
+ * builder.writeln(fillIns.get(ans-1)); builder.writeln()
  *
  */
 
@@ -38,48 +40,72 @@ public class appView {
 	class TabHolder {
 		private String name;
 		private ArrayList<QGroup> questions;
-		
+
 		public TabHolder(String name, ArrayList<QGroup> questions) {
 			this.name = name;
 			this.questions = questions;
 		}
-		public String getName() {return name;}
-		public ArrayList<QGroup> getQuestions() {return questions;}
+
+		public String getName() {
+			return name;
+		}
+
+		public ArrayList<QGroup> getQuestions() {
+			return questions;
+		}
 	}
-	
+
 	class QGroup {
 		private String question;
 		private ArrayList<answerSelections> answers;
-		
+
 		public QGroup(String question, ArrayList<answerSelections> answers) {
 			this.question = question;
 			this.answers = answers;
 		}
-		public String getQuestion() {return question;}
-		public ArrayList<answerSelections> getAnswers() {return answers;}
+
+		public String getQuestion() {
+			return question;
+		}
+
+		public ArrayList<answerSelections> getAnswers() {
+			return answers;
+		}
 	}
-	
+
 	class answerSelections {
 		private String answer;
 		private String fillIn;
 		private Button button;
-		
+
 		public answerSelections(String answer, String fillIn) {
 			this.answer = answer;
-			this.fillIn= fillIn;
+			this.fillIn = fillIn;
 			this.button = null;
 		}
-		public String getAnswer() {return answer;}
-		public String getfillIn() {return fillIn;}
-		public Button getButton() {return button;}
-		
-		public void setButton(Button button) {this.button = button;}
+
+		public String getAnswer() {
+			return answer;
+		}
+
+		public String getfillIn() {
+			return fillIn;
+		}
+
+		public Button getButton() {
+			return button;
+		}
+
+		public void setButton(Button button) {
+			this.button = button;
+		}
 	}
-	
+
 	protected Shell shell;
 
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -117,32 +143,53 @@ public class appView {
 		ArrayList<TabHolder> tabList = parseInput();
 		System.out.println("End parse");
 
-		
-		TabFolder tabFolder = new TabFolder(shell, SWT.NONE);
+		TabFolder tabFolder = new TabFolder(shell, SWT.H_SCROLL | SWT.V_SCROLL);
 		tabFolder.setBounds(0, 10, 464, 455);
-		
+
 		for (int i = 0; i < tabList.size(); i++) {
-			TabItem tabItem_1 = new TabItem(tabFolder, SWT.NONE);
-			tabItem_1.setText(tabList.get(i).getName());
+
 			ArrayList<QGroup> questionList = tabList.get(i).getQuestions();
-			Composite composite = new Composite(tabFolder, SWT.NONE);
+
+			ScrolledComposite scroll = new ScrolledComposite(tabFolder,
+					SWT.V_SCROLL | SWT.BORDER);
+			Composite composite = new Composite(scroll, SWT.NONE);
+
+			scroll.setContent(composite);
+			scroll.setExpandHorizontal(true);
+			scroll.setExpandVertical(true);
+			scroll.addControlListener(new ControlAdapter() {
+				public void controlResized(ControlEvent e) {
+					Rectangle r = scroll.getClientArea();
+					scroll.setMinSize(composite.computeSize(r.width,
+							SWT.DEFAULT));
+				}
+			});
+
 			composite.setLayout(new GridLayout(1, false));
 
-			for(int j = 0; j < questionList.size(); j++) {
+			for (int j = 0; j < questionList.size(); j++) {
 				Group group = new Group(composite, SWT.NONE);
-				GridData gd_group = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+				GridData gd_group = new GridData(SWT.LEFT, SWT.CENTER, false,
+						false, 1, 1);
 				gd_group.widthHint = 441;
 				group.setLayoutData(gd_group);
 				group.setText(questionList.get(j).getQuestion());
 				group.setLayout(new GridLayout(1, false));
-				
-				for(int k = 0; k < questionList.get(j).getAnswers().size(); k++) {
+
+				for (int k = 0; k < questionList.get(j).getAnswers().size(); k++) {
 					Button button = new Button(group, SWT.RADIO);
-					button.setText(questionList.get(j).getAnswers().get(k).getAnswer());
-					tabList.get(i).getQuestions().get(j).getAnswers().get(k).setButton(button);
+					button.setText(questionList.get(j).getAnswers().get(k)
+							.getAnswer());
+					tabList.get(i).getQuestions().get(j).getAnswers().get(k)
+							.setButton(button);
 				}
 			}
-			tabItem_1.setControl(composite);
+
+			// Expand both horizontally and vertically
+
+			TabItem tabItem_1 = new TabItem(tabFolder, SWT.NONE);
+			tabItem_1.setText(tabList.get(i).getName());
+			tabItem_1.setControl(scroll);
 		}
 
 		Button btnCreateDocument = new Button(shell, SWT.NONE);
@@ -152,21 +199,25 @@ public class appView {
 				for (int i = 0; i < tabList.size(); i++) {
 					int questionSize = tabList.get(i).getQuestions().size();
 					for (int j = 0; j < questionSize; j++) {
-						int answerSize = tabList.get(i).getQuestions().get(j).getAnswers().size();
-						for(int k = 0; k < answerSize; k++) {
-							if (tabList.get(i).getQuestions().get(j).getAnswers().get(k).getButton().getSelection()) {
-								System.out.println(tabList.get(i).getQuestions().get(j).getAnswers().get(k).getfillIn());
+						int answerSize = tabList.get(i).getQuestions().get(j)
+								.getAnswers().size();
+						for (int k = 0; k < answerSize; k++) {
+							if (tabList.get(i).getQuestions().get(j)
+									.getAnswers().get(k).getButton()
+									.getSelection()) {
+								System.out.println("For Question: " + tabList.get(i).getQuestions().get(j).getQuestion() + ", you selected " + tabList.get(i).getQuestions().get(j).getAnswers().get(k).getAnswer() + " which corresponds to ... " + 
+										tabList.get(i).getQuestions().get(j).getAnswers().get(k).getfillIn());
 							}
 						}
 					}
 				}
-				
+
 			}
 		});
 		btnCreateDocument.setBounds(326, 471, 128, 25);
-		btnCreateDocument.setText("Create Document");	
+		btnCreateDocument.setText("Create Document");
 	}
-	
+
 	public ArrayList<TabHolder> parseInput() {
 		String filename = "test1.txt";
 		File file = new File(filename);
@@ -175,33 +226,35 @@ public class appView {
 			FileReader reader = new FileReader(file);
 			BufferedReader textReader = new BufferedReader(reader);
 			String line = textReader.readLine();
-			if (line == null) 
+			if (line == null)
 				System.exit(0);
 			// ...until there is nothing else to read
 			ArrayList<TabHolder> retVal = new ArrayList<TabHolder>();
 
-			while(!line.equals("END")) {	
-				//start tab
+			while (!line.equals("END")) {
+				// start tab
 				if (line.equals("*--")) {
 					line = textReader.readLine();
 					TabHolder tab = new TabHolder(line, new ArrayList<QGroup>());
 					line = textReader.readLine();
 					while (!line.equals("*--")) {
-						//if true start new question
+						// if true start new question
 						if (line.equals("---")) {
 							System.out.println("question");
 							line = textReader.readLine();
-							QGroup g = new QGroup(line, new ArrayList<answerSelections>());
+							QGroup g = new QGroup(line,
+									new ArrayList<answerSelections>());
 							line = textReader.readLine();
-							while(!line.equals("^^^")) {
+							while (!line.equals("^^^")) {
 								String answer = line;
 								line = textReader.readLine();
 								String fillIn = line;
-								g.getAnswers().add(new answerSelections(answer, fillIn));
+								g.getAnswers().add(
+										new answerSelections(answer, fillIn));
 								line = textReader.readLine();
 							}
 							tab.getQuestions().add(g);
-							line = textReader.readLine();		
+							line = textReader.readLine();
 						}
 					}
 					retVal.add(tab);
@@ -209,11 +262,10 @@ public class appView {
 				line = textReader.readLine();
 			}
 			return retVal;
-		}	
-		catch (IOException x) {
-		    System.err.println(x);
+		} catch (IOException x) {
+			System.err.println(x);
 		}
-		
-		return null; 
+
+		return null;
 	}
 }
